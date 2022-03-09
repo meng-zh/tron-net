4f80d3997b24d35676eec5e6d8d9675232697336
meng <zhengzhengzhengmm@126.com>
2022-03-09 17:35:30 +0800

    tron address

--- examples/types_test.rs
+++ examples/types_test.rs
@@ -3,11 +3,7 @@ use tron_net::types::{from_base58, from_hex, Address};
 
 fn main() {
     let from = "TJnGao5hYkJxSxs2vm7orE5pxHQf2ofQQU";
-    // let bytes = from_base58(from).expect("error");
-    // let address = from_hex(&bytes,0x41);
     let address2 = Address::from_base58(from);
-   let a = address2.to_base58();
-    println!("{}",a);
-    println!("{}",address2);
-    // assert_eq!(address,from);
+    let a = address2.to_base58();
+    assert_eq!(address, a);
 }
